961a988106eef81d6c789a823ce74d0df59dce01
Maryna Andreieva <maryna_andreieva@iownit.us>
2022-11-23 14:25:51 +0200

    Updated tasks for the third homework

--- src/com/company/fourthHomeWorkOverloadedMethods/Employee.java
+++ src/com/company/fourthHomeWorkOverloadedMethods/Employee.java
@@ -2,14 +2,24 @@ package com.company.fourthHomeWorkOverloadedMethods;
 
 public class Employee {
 
+    private final int id;
+    private final String sureName;
+    private final int age;
     private final int salary;
+    private final String department;
 
     public Employee(int id, String sureName, int age, int salary, String department) {
+        this.id = id;
+        this.sureName = sureName;
+        this.age = age;
         this.salary = salary;
+        this.department = department;
     }
 
     public void doubleSalary() {
         int doubleSalary = salary * 2;
-        System.out.println(doubleSalary);
+        System.out.println("Employee id, surname, age, salary, department: " + id + " and " + sureName);
+        System.out.println("Employee age, salary, department: " + age + " and " + salary + " and " + department);
+        System.out.println("Employee doubled salary: " + doubleSalary);
     }
 }
--- src/com/company/thirdHomeWork/StudentTest.java
+++ src/com/company/thirdHomeWork/StudentTest.java
@@ -2,44 +2,14 @@ package com.company.thirdHomeWork;
 
 public class StudentTest {
 
-    private static int studentIdNumber;
-    private static int studentCourse;
-    private static int averageValueMath;
-    private static int averageValueEconomics;
-    private static int averageValueForeignLanguage;
-    private int averageValueOfThreeSubjects;
-    private static String studentSureName;
-    private static String studentName;
-
-    public StudentTest(int averageValueMath, int averageValueEconomics, int averageValueForeignLanguage, String studentName, String studentSureName, int studentCourse, int studentIdNumber) {
-        StudentTest.averageValueMath = averageValueMath;
-        StudentTest.averageValueEconomics = averageValueEconomics;
-        StudentTest.averageValueForeignLanguage = averageValueForeignLanguage;
-        StudentTest.studentName = studentName;
-        StudentTest.studentSureName = studentSureName;
-        StudentTest.studentCourse = studentCourse;
-        StudentTest.studentIdNumber = studentIdNumber;
-    }
-
     public static void main(String[] args) {
+        Student studentOne = new Student(9, 10, 12, "Petr", "Karasov", 2, 367);
+        studentOne.averageValueOfThreeSubjects (studentOne);
 
-        StudentTest studentOne = new StudentTest(9, 10, 12, "Petr", "Karasov", 2, 367);
-        studentOne.averageValueOfThreeSubjects = (averageValueMath + averageValueEconomics + averageValueForeignLanguage) / 3;
-        System.out.println("Student's name and surname: " + studentName + " " + studentSureName);
-        System.out.println("Student's course and ID Number: " + studentCourse + " and " + studentIdNumber);
-        System.out.println("Student's average value of three subjects: " + studentOne.averageValueOfThreeSubjects);
+        Student studentTwo = new Student(8, 9, 8, "Viktor", "Nevdiachnyy", 4, 645);
+        studentTwo.averageValueOfThreeSubjects (studentTwo);
 
-        StudentTest studentTwo = new StudentTest(8, 9, 8, "Viktor", "Nevdiachnyy", 4, 645);
-        studentTwo.averageValueOfThreeSubjects = (averageValueMath + averageValueEconomics + averageValueForeignLanguage) / 3;
-        System.out.println("Student's name and surname: " + studentName + " " + studentSureName);
-        System.out.println("Student's course and ID Number: " + studentCourse + " and " + studentIdNumber);
-        System.out.println("Student's average value of three subjects: " + studentTwo.averageValueOfThreeSubjects);
-
-        StudentTest studentThree = new StudentTest(5, 8, 11, "Sergiy", "Velykiy", 1, 23);
-        studentThree.averageValueOfThreeSubjects = (averageValueMath + averageValueEconomics + averageValueForeignLanguage) / 3;
-        System.out.println("Student's name and surname: " + studentName + " " + studentSureName);
-        System.out.println("Student's course and ID Number: " + studentCourse + " and " + studentIdNumber);
-        System.out.println("Student's average value of three subjects: " + studentThree.averageValueOfThreeSubjects);
+        Student studentThree = new Student(5, 8, 11, "Sergiy", "Velykiy", 1, 23);
+        studentThree.averageValueOfThreeSubjects (studentThree);
     }
 }
-
