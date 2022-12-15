67dc970b174ecdbc431728e0394dc51b79230e01
Maryna Andreieva <maryna_andreieva@iownit.us>
2022-12-15 10:22:20 +0200

    7th HM - not finished yet (second attempt)

--- src/com/company/thirdHomeWork/StudentTest.java
+++ src/com/company/thirdHomeWork/StudentTest.java
@@ -2,6 +2,9 @@ package com.company.thirdHomeWork;
 
 public class StudentTest {
 
+private int course;
+private int amountOfStudentsOnCourse;
+
     public static void main(String[] args) {
         Student studentOne = new Student(9, 10, 12, "Petr", "Karasov", 2, 367);
         studentOne.averageValueOfThreeSubjects(studentOne);
@@ -16,8 +19,15 @@ public class StudentTest {
 
         Student student2 = new Student();
 
-        public void method(Student student){
-            if () {
+
+        public static void method1(){
+            Student studentA = new Student (3, 15, 4, "Viktor", "Pavlic", 4, 2342);
+            studentA.averageValueOfThreeSubjects(studentA);
+
+            Student studentB = new Student (3, 15, 4, "Oleksiy", "Potapenko", 5, 3434);
+            studentA.averageValueOfThreeSubjects(studentB);
+            if (studentA.course <= studentA.course) {
+
                 System.out.println();
             }
         }
